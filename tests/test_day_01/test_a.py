32cfb427077d1eb1a3f0d507ad52264d256e1274
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-12-01 07:43:17 -0500

    Namespace a solutions

--- tests/test_day_01/test_a.py
+++ tests/test_day_01/test_a.py
@@ -1,13 +1,17 @@
 import pytest
 
 from aoc_2024.day_01.a import get_solution, solve
-from aoc_2024.day_01.from_prompt import ACTUAL_SOLUTION, SAMPLE_DATA, SAMPLE_SOLUTION
+from aoc_2024.day_01.from_prompt import (
+    ACTUAL_SOLUTION_A,
+    SAMPLE_DATA,
+    SAMPLE_SOLUTION_A,
+)
 
 
 def test_solve():
-    assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION
+    assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_A
 
 
 @pytest.mark.skip
 def test_my_solution():
-    assert get_solution() == ACTUAL_SOLUTION
+    assert get_solution() == ACTUAL_SOLUTION_A
