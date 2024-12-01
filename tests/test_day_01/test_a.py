32f527cdf839b526b3d36903e775ef5878e1d464
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-12-01 07:44:54 -0500

    Unskip solution a

--- tests/test_day_01/test_a.py
+++ tests/test_day_01/test_a.py
@@ -1,5 +1,3 @@
-import pytest
-
 from aoc_2024.day_01.a import get_solution, solve
 from aoc_2024.day_01.from_prompt import (
     ACTUAL_SOLUTION_A,
@@ -12,6 +10,5 @@ def test_solve():
     assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_A
 
 
-@pytest.mark.skip
 def test_my_solution():
     assert get_solution() == ACTUAL_SOLUTION_A
