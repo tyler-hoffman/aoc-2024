afe86afef8ffd4164d216c252749c9e843b6a3d6
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-12-01 08:16:00 -0500

    Remove sample test

--- aoc_2024/utils/wat.py
+++
@@ -1,2 +0,0 @@
-def wat() -> str:
-    return "wat"
--- tests/utils/test_wat.py
+++
@@ -1,6 +0,0 @@
-from aoc_2024.utils.wat import wat
-from tests.utils.from_prompt import EXPECTED_WAT_VALUE
-
-
-def test_wat() -> None:
-    assert wat() == EXPECTED_WAT_VALUE
