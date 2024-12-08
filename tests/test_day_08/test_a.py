1c0c9dd847a787c347fd20a79f7729ffc5bcbea2
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-12-08 08:19:18 -0500

    Solve 08.a

---
+++ tests/test_day_08/test_a.py
@@ -0,0 +1,10 @@
+from aoc_2024.day_08.a import get_solution, solve
+from aoc_2024.day_08.from_prompt import SAMPLE_DATA, SAMPLE_SOLUTION_A, SOLUTION_A
+
+
+def test_solve():
+    assert solve(SAMPLE_DATA) == SAMPLE_SOLUTION_A
+
+
+def test_my_solution():
+    assert get_solution() == SOLUTION_A
